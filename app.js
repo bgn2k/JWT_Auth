c2240b31d0ba8694c27594455c436bb860d16ec0
B Govinda Narendra <115526943+bgn2k@users.noreply.github.com>
2024-04-28 20:36:31 +0530

    Added login logic and updated the html (#1)
    * Added login logic
    
    * Added Logout & Conditional Rendering

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const cookies = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 const app = express();
 
 //middleware
@@ -33,5 +34,6 @@ mongoose
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser)
 app.get("/", (req, res) => res.render("home"));
-app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.get("/smoothies",requireAuth, (req, res) => res.render("smoothies"));
--- controller/authController.js
+++ controller/authController.js
@@ -5,6 +5,17 @@ const cookieParser = require("cookie-parser");
 const handleError = (err) => {
   console.log(err.message, err.code)
   let errors = {email : '', password : ''}
+
+  //incorrect email : 
+if(err.message === 'incorrect email')
+{
+  errors.email = 'User is not registered. Kindly sign in'
+}
+if(err.message === 'incorrect password')
+{
+  errors.password = 'Invalid credentials! Kindly check again'
+}
+
   if(err.code === 11000){
       errors.email = 'User already exists! Please try again'
     return errors
@@ -17,7 +28,7 @@ const handleError = (err) => {
   return errors
 }
 //days * hours * mins * secs
-const maxExpiryLimit = 2 * 24 * 60 * 60
+const maxExpiryLimit = 3 * 60 * 60
 const createToken = (id) => {
   return jwt.sign({id}, 'life sucks', {
     expiresIn : maxExpiryLimit
@@ -34,7 +45,7 @@ const signup_post = async (req, res) => {
     const user = await User.create({ email, password });
     const token = createToken(user._id)
     res.cookie('jwt', token, {httpOnly : true,maxAge : maxExpiryLimit * 1000})
-    res.status(201).json({user});
+    res.status(201).json({user : user._id});
   } catch (err) {
     const errBody = handleError(err)
     res.status(500).json({errBody});
@@ -44,14 +55,32 @@ const signup_post = async (req, res) => {
 const login_get = async (req, res) => {
   res.render("login");
 };
+
 const login_post = async (req, res) => {
-  const { email, password } = req.body;
-  res.send(`Welcome ${data.username}!\nYou are successfully logged in!`);
+  const { email, password } = req.body;  
+  console.log('Email & Password form UI in here = ', email, password)
+  try {
+    const user = await User.login(email, password)
+    console.log({user : user._id})
+    const token = createToken(user._id)
+    res.cookie('jwt', token, {httpOnly : true, maxExpiryLimit})
+    res.status(200).json({user : user._id})
+  } catch (error) {
+    console.log('Something wrong here when clicked on the login button')
+    const errors = handleError(error)
+    res.status(400).json({errors})
+  }
 };
+
+const logout_get = async (req, res) => {
+  res.cookie('jwt', '', {maxExpiryLimit : 1})
+  res.redirect('/login')
+}
 
 module.exports = {
   signup_get,
   signup_post,
   login_get,
   login_post,
+  logout_get
 };
